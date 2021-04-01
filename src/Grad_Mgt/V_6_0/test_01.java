3d621f68fa12556c7a8da17a5ffb24ac401da39c
user <user@LAPTOP-070RFMDH>
2021-04-01 18:19:58 +0900

    원하는 수 만큼 출력 하기

---
+++ src/Grad_Mgt/V_6_0/test_01.java
@@ -0,0 +1,26 @@
+package Grad_Mgt.V_6_0;
+
+import java.util.Scanner;
+
+public class test_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("몇명의 성적표를 만들까요?");
+		int su = sc.nextInt();
+
+		int[] su2 = new int[su];
+		int cont = 0;
+
+		for (int i = 0; i < su2.length; i++) {
+			System.out.println("원하는 숫자 입력하세요");
+			su2[i] = sc.nextInt();
+		}
+		for (int k : su2) {
+			System.out.println(k);
+		}
+
+	}
+
+}
