7d69cd6f2392aefb44337c52d0fea10dc642fe6e
marin <marin@DESKTOP-6I6B65R>
2021-04-02 00:56:17 +0900

    평균 , 학점 배열 적용 안됨 에러 남 수정 해야함

---
+++ src/Grad_Mgt/src/Grad_Mgt/V_6_0/MainEntry.java
@@ -0,0 +1,139 @@
+/*
+성적관리프로그램_V_6_0
+subject: 성적처리를 배열로 입력 받아 3명의 점수를 출력
+update list:
+배열 처리 하기 
+
+ Date : 04_01_21
+
+
+*/
+
+package Grad_Mgt.V_6_0;
+
+import java.util.Scanner;
+
+public class MainEntry {
+	public static void main(String[] args) {
+
+//		rusult>
+//		국어 : 100 , 영어 :100 ,전산 : 100
+//		총점 : 300 , 평균 :100.00 , 학점 :
+		// 성적관리 프로그램_V_6.0 //
+
+		/*
+		 * 변수 선언 kor(int) = 국어 eng(int) = 영어 com(int) = 전산 / total(int) = 점수 합계 grade
+		 * (char) = 학점
+		 * 
+		 */
+		
+		
+		
+
+
+		Scanner sc = new Scanner(System.in);
+		
+	
+		System.out.println("성적관리 프로그램_V_6.0 _ 배열");
+		// V_5_0
+		// 수정 사항//배열
+		
+		// 몇명의 성적표를 만들건지 
+		System.out.println("몇명의 성적표를 만들까요?");
+		 int grade_card_num = sc.nextInt();
+		 
+		String[][]name = new String[grade_card_num][1];
+		int kor, eng, com, total,i = 0, j=0;
+		int[][] score = new int[grade_card_num][4];
+		char[][] grade = new char[grade_card_num][1];
+		//double[] avg = new double[2];
+		float[] avg = new float[2];
+	
+		
+		while (i < grade_card_num) { 
+			 j = 0;
+			System.out.print((i+1)+"번째 학생 "+"이름을 입력 해주세요 ");
+				name[i][0] = sc.next();
+
+			do {
+				System.out.print("국어 점수 입력 해주세요 : ");
+				kor = sc.nextInt();
+				score[i][j] = kor;
+			} while (kor < 0 || kor > 100);
+			j++;
+			do 
+			{
+				System.out.print("영어 입력 해주세요 : ");
+				eng = sc.nextInt();
+				score[i][j] = eng;
+			} while (eng < 0 || eng > 100);
+			j++;
+			do 
+			{
+				System.out.print("컴퓨터 입력 해주세요 : ");
+				com = sc.nextInt();
+				score[i][j] = com;
+			} while (com < 0 || com > 100);
+			j++;
+			
+			total = kor + eng + com;
+			score[i][j] = total;
+
+			avg[i] = (float) (score[i][j] / 3.0); 
+
+			//avg = (((float) total) / 3);
+//
+//			if (avg[i] >= 90) {
+//				grade[grade_card_num][1] = 'A';
+//			} else if (avg[i] >= 80) {
+//				grade[grade_card_num][1] = 'B';
+//			} else if (avg[i] >= 70) {
+//				grade[grade_card_num][1] = 'C';
+//			} else if (avg[i] >= 60) {
+//				grade[grade_card_num][1] = 'D';
+//			} else {
+//				grade[grade_card_num][1] = 'F';
+//			}
+			i++;
+		}//while out
+		
+		//System.out.println("*****성적표******");
+		for (int k = 0; k < grade_card_num; k++) {
+			System.out.println((k+1)+"번 학생 "+name[k][0]+" 성적표 *********");
+			System.out.println("국어 : " + score[k][0] + " 영어 : " + score[k][1] + " 전산 : " + score[k][2]);
+	//
+			System.out.print("총점 : " + score[k][3]+ "\n");
+			//
+			//System.out.printf(" 평균 : %.2f",  avg[k]+ "\n");
+			//System.out.print(" 학점" +grade[k][0]);
+//			
+//			System.out.println();
+		}// for from k out
+	
+	}
+}
+// 지워도 됨
+////try 문 안으로 들어 //합격여부
+//
+//try {
+//
+//	total = (kor + eng + com);
+//	avg = (((float) total) / 3);
+//
+//	if (avg < 60) {
+//		System.out.println("불합격 입니다");
+//	}
+//	if (kor < 40) {
+//		System.out.println("국어 불합격" + kor + "점");
+//	}
+//	if (eng < 40) {
+//		System.out.println("영어 불합격" + eng + "점");
+//	}
+//	if (com < 40) {
+//		System.out.println("전산 불합격" + com + "점");
+//	} else {
+//		System.out.println("축하드립니다 평균 점수" + avg + "점 으로 합격 입니다");
+//	}
+//} catch (Exception e) {
+//	System.out.println("숫자만 입력 해주세요");
+//}
