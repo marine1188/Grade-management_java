1a60a0634a92ec2eb3faf0929a6464bf82b06aee
user <user@LAPTOP-070RFMDH>
2021-04-02 17:50:27 +0900

    V_1_1// 클래스 분리 해야함

---
+++ src/Grad_Mgt/V_1_1/MainEntry.java
@@ -0,0 +1,29 @@
+/*
+ Grad_Mgt.V_1_1
+ 클래스로 구분하기
+  코딩 중 
+ */
+//package Grad_Mgt.V_1_1;
+//
+//public class MainEntry {
+//	public static void main(String[] args) {
+//		// 성적관리프로그램 v1_0
+//		//국어,영어,전산 점수 임의적으로 부여하고 총점 / 평균 까지 출력
+//	    //kor = 국어 / en = 영어 / com = 전산
+//			int kor = 90;
+//			int en = 100;
+//			int com = 70;
+//			// 국어,영어,전산 점수의 총점을 나타 내는 변수 생성 // total
+//			int total = (kor+en+com);
+//			
+//			System.out.println("성적관리 프로그램_v1.0");
+//			System.out.println("----------------");
+//			System.out.println("국어 점수" +"=" +kor);
+//			System.out.println("영어 점수" +"=" +en);
+//			System.out.println("전산 점수" +"=" +com);
+//			System.out.println("----------------");
+//			System.out.println("점수 총점"+"="+ total);
+//			System.out.println("점수 평균"+"="+(total/3));
+//			
+//	}
+//}
